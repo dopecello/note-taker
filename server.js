d6f135ff3fcbfbcd1cef9b344cd06a0e6d51cfc4
dopecello <williamlocke.cello@gmail.com>
2022-11-02 16:29:21 -0400

    note posting route works

--- server.js
+++ server.js
@@ -3,9 +3,17 @@ const notes = require("./db/db.json")
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.get("/api/notes", (req, res) => {
   res.json(notes);
 });
+
+app.post("/api/notes", (req, res) => {
+    console.log(req.body)
+    res.json(req.body)
+})
 
 app.listen(PORT, () => {
   console.log(`Notetaking occurring on port ${PORT}`);
